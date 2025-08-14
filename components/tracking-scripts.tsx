5ace1c632ad41a6350a831f34835529a709c7b22
ncheyer <noahj@cheyer.com>
2025-08-14 18:56:13 +1000

    Add Umami analytics tracking
    - Added Umami analytics script to TrackingScripts component
    - Website ID: e9883970-17ec-4067-a92a-a32cfe6a36d0
    - Script loads with afterInteractive strategy for optimal performance

--- components/tracking-scripts.tsx
+++ components/tracking-scripts.tsx
@@ -20,28 +20,32 @@ export default function TrackingScripts({ trackingCodes }: TrackingScriptsProps)
     // For example: window.gtag('event', 'page_view', { page_path: pathname });
   }, [pathname])
 
-  if (!trackingCodes) {
-    return null
-  }
-
   return (
     <>
-      {/* Scripts for the <head> section */}
-      {trackingCodes.head && (
+      {/* Umami Analytics */}
+      <Script
+        defer
+        src="https://cloud.umami.is/script.js"
+        data-website-id="e9883970-17ec-4067-a92a-a32cfe6a36d0"
+        strategy="afterInteractive"
+      />
+
+      {/* Custom tracking codes if provided */}
+      {trackingCodes?.head && (
         <Script id="tracking-head-script" strategy="afterInteractive">
           {trackingCodes.head}
         </Script>
       )}
 
       {/* Scripts for the start of <body> */}
-      {trackingCodes.bodyStart && (
+      {trackingCodes?.bodyStart && (
         <Script id="tracking-body-start-script" strategy="beforeInteractive">
           {trackingCodes.bodyStart}
         </Script>
       )}
 
       {/* Scripts for the end of <body> */}
-      {trackingCodes.bodyEnd && (
+      {trackingCodes?.bodyEnd && (
         <Script id="tracking-body-end-script" strategy="lazyOnload">
           {trackingCodes.bodyEnd}
         </Script>
